3bfa68ac32dedccd55f74e727b4adfd344c57a6d
rosfort65 <rosfort98hdj11@gmail.com>
2025-05-17 11:18:14 +0200

    Updated middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  defaultLocale: process.env.NEXT_PUBLIC_DEFAULT_REGION || 'gb',
+});
 
 export const config = {
   matcher: [
     '/',
-    `/(gb)/:path*`,
+    `/(${process.env.NEXT_PUBLIC_DEFAULT_REGION || 'gb'})/:path*`,
     '/((?!api|_next|_vercel|.*\\..*).*)',
   ],
 };
